c3f7a7a61fef54bec2e0f6c3cb4760b2dbb8b7ab
Barbie <barbie@missbarbell.co.uk>
2013-04-07 14:21:23 +0100

    documentation updates

--- lib/Test/CPAN/Meta/JSON.pm
+++ lib/Test/CPAN/Meta/JSON.pm
@@ -224,7 +224,7 @@ for Miss Barbell Productions, L<http://www.missbarbell.co.uk>
 
   Copyright (C) 2009-2012 Barbie for Miss Barbell Productions
 
-  This module is free software; you can redistribute it and/or
+  This distribution is free software; you can redistribute it and/or
   modify it under the Artistic Licence v2.
 
 =cut
